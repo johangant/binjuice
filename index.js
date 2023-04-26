0e93ef95f9a9ffe09c29543fa29602c00674abae
Johan Gant <johan@jnca-consulting.com>
2023-04-26 14:47:34 +0100

    Update to use smtp mailer rather than webhook

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const cheerio = require('cheerio');
 const normalizeWhitespace = require('normalize-html-whitespace');
 const TurndownService = require('turndown')
 
+require('dotenv').config();
+const nodemailer = require("nodemailer");
+
 const destUrl = process.env.WEBHOOK;
 const sourceUrl = process.env.SOURCE_URL;
+const apiPass = process.env.SENDGRID_API_KEY;
+const toEmail = process.env.TO_EMAIL;
+const fromEmail = process.env.FROM_EMAIL;
 
 function getSourceData(url) {
   return axios.get(url)
@@ -31,10 +37,35 @@ function postToWebhook(url, data) {
     });
 }
 
+function sendEmailReport(data, apiPass, toEmail, fromEmail) {
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.sendgrid.net',
+    port: 465,
+    auth: {
+        user: "apikey",
+        pass: apiPass
+    }
+  });
+
+  transporter.sendMail({
+    from: 'BinJuice <' + fromEmail + '>', // verified sender email
+    to: toEmail, // recipient email
+    subject: "Bin collection for the week", // Subject line
+    text: data // plain text body,
+  }, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+}
+
 getSourceData(sourceUrl)
   .then(data => {
-    postToWebhook(destUrl, data)
-      .then(data => {
-        console.log(data);
-      });
+    sendEmailReport(data, apiPass, toEmail, fromEmail)      
+    // postToWebhook(destUrl, data)
+      // .then(data => {
+      //   console.log(data);
+      // });
   });
